30ba9edb013a54c7edcd3f5ce34e94a6411aaf17
ALittleDaisy <1152594956@qq.com>
2023-04-13 18:15:53 +0800

    能去选择手机中的文件 ，但是保存功能没做

--- app/src/main/java/com/example/storagetest/MainActivity.kt
+++ app/src/main/java/com/example/storagetest/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.storagetest
 
 import android.Manifest
+import android.app.Activity
 import android.content.ContentUris
 import android.content.ContentValues
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -23,6 +25,8 @@ import java.io.BufferedOutputStream
 import java.net.HttpURLConnection
 import java.net.URL
 import kotlin.concurrent.thread
+
+const val PICK_FILE = 1
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -125,6 +129,12 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        binding.btnPickFile.setOnClickListener {
+            val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+            intent.addCategory(Intent.CATEGORY_OPENABLE)
+            intent.type = "*/*"
+            startActivityForResult(intent, PICK_FILE)
+        }
     }
 
     private fun checkPermission() {
@@ -149,6 +159,39 @@ class MainActivity : AppCompatActivity() {
                     finish()
                 }
             }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            PICK_FILE -> {
+                if (resultCode == Activity.RESULT_OK && data != null) {
+                    val uri = data.data  //获得选择的文件的uri
+                    if (uri != null) {
+                        val fileName = getFileNameByUri(uri)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun getFileNameByUri(uri: Uri): String {
+        var fileName = System.currentTimeMillis().toString()
+        val cursor = contentResolver.query(uri, null, null, null, null)
+        if (cursor != null && cursor.count > 0) {
+            cursor.moveToFirst()
+            fileName = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DISPLAY_NAME))
+            Log.d("Daisy", "看看uri吧 ${cursor} ，文件名 $fileName")
+            cursor.close()
+        }
+        return fileName
+    }
+
+    private fun copyUriToExternalFileDir(uri: Uri, fileName: String) {
+        thread {
+            val inputStream = contentResolver.openInputStream(uri)
+            val tempDir = getExternalFilesDir("temp")
         }
     }
 }
